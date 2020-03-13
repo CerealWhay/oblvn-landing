d0828b87f83a664e384e4acd7c32b2e74ab9cae3
Ivan <cerealwhay@gmail.com>
2020-03-13 19:33:32 +1000

    first commit

---
+++ mail.php
@@ -0,0 +1,13 @@
+<?php
+
+$recepient = "xojiogell@gmail.com";
+$siteName = "OBLIVION rec";
+
+// $name = trim($_POST["name"]);
+$phone = trim($_POST["phone"]);
+$message = "Контакты: $phone";
+
+$pagetitle = "Заявка с сайта \"$siteName\"";
+mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
+
+?>
